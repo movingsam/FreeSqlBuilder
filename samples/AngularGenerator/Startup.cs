f8559b43e407265e37253aabcb884c8cc523f2b5
samsu910104 <samsu910104@hotmail.com>
2020-04-04 14:21:04 +0800

    整理项目结构

---
+++ samples/AngularGenerator/Startup.cs
@@ -0,0 +1,69 @@
+using FreeSql.Generator;
+using FreeSql.Generator.Core;
+using FreeSql.GeneratorUI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.FileProviders.Physical;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace AngularGenerator
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(opt => opt.EnableEndpointRouting = false)
+                .AddNewtonsoftJson(option => option.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore); ;
+            services.AddFreeSqlGen(option =>
+            {
+                option.DefaultTemplatePath = "Template";
+                option.SqliteDbConnectionString = @"Data Source=D:\Template\generator.db;Version=3";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseCors("cors");
+            app.UseStaticFiles();
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}");
+
+            });
+            app.UseFreeSqlGenUI(opt =>
+            {
+                opt.Path = "CodeGenerator";
+            });
+            //app.UseSpa(x => x.UseProxyToSpaDevelopmentServer("http://localhost:4200"));
+        }
+    }
+}
---
+++ src/FsGen/FreeSql.Generator/GeneratorGenExtensions.cs
@@ -0,0 +1,69 @@
+﻿using AngularGenerator.Services;
+using FreeSql.Aop;
+using FreeSql.Generator.Core;
+using FreeSql.Generator.Helper;
+using FreeSql.TemplateEngine;
+using FreeSql.TemplateEngine.Implement;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Encodings.Web;
+
+namespace FreeSql.Generator
+{
+    /// <summary>
+    /// 代码生成器服务相关拓展
+    /// </summary>
+    public static class FreeSqlGenExtensions
+    {
+        /// <summary>
+        /// 添加FreeSqlGen相关
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="setupAction">生成器模板配置项</param>
+        public static void AddFreeSqlGen(this IServiceCollection services, Action<GenTemplateOptions> setupAction = null)
+        {
+            var options = new GenTemplateOptions();
+            setupAction?.Invoke(options);
+            services.AddSingleton(options);//配置导入
+            services.AddRazorPages().AddRazorRuntimeCompilation();//MVC动态编译
+            services.AddScoped<IProjectService, ProjectService>();//项目核心服务
+            services.AddSingleton<HtmlEncoder>(NullHtmlEncoder.Default);//HTML中文乱码
+            services.AddSingleton<FileProviderHelper>();//文件相关处理
+            services.AddSingleton<IFreeSql<FsGen>>(x =>
+                {
+                    var builder = new FreeSqlBuilder()
+                                .UseConnectionString(dataType: DataType.Sqlite, options.SqliteDbConnectionString)
+                                .UseAutoSyncStructure(true)
+                                .Build<FsGen>();
+                    builder.Aop.CommandAfter += (s, e) => Aop_CommandAfter(s, e, x.GetService<ILogger<IFreeSql>>());
+                    return builder;
+                });//持久化
+            services.AddScoped<ReflectionHelper>();//反射助手
+            services.AddScoped<BuildTask>();//核心任务
+            services.AddSingleton<RazorTemplateEngine>();//Razor模板引擎
+            services.AddTransient<RazorViewToStringRender>();//Razor渲染字符串工具
+            var fileProvider = services.BuildServiceProvider().GetRequiredService<FileProviderHelper>();
+            fileProvider.CopyToProjectRoot();//拷贝模板到根目录
+            fileProvider.InitTemplate();//导入模板到数据库
+        }
+        /// <summary>
+        /// sql监控
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <param name="logger"></param>
+        private static void Aop_CommandAfter(object sender, FreeSql.Aop.CommandAfterEventArgs e, ILogger logger)
+        {
+            logger.LogInformation($"======================执行耗时:{e.ElapsedMilliseconds.ToString()}======================");
+            logger.LogInformation($"======================执行语句======================");
+            logger.LogInformation($"{e.Log}");
+            logger.LogInformation($"======================执行语句======================");
+        }
+
+    }
+}
